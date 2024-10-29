f8ae1199603f4ef9aefae643de7a2da6ac144ca2
Marius Gafton <mariusgafton2005@gmail.com>
2024-10-29 23:11:01 +0100

    Initial commit

---
+++ QuickShiftApplication/Services/ItemService.cs
@@ -0,0 +1,16 @@
+﻿namespace QuickShiftApplication.Services;
+
+public class ItemService
+{
+    private readonly List<string> _items = new List<string>();
+
+    public void AddItem(string item)
+    {
+        _items.Add(item);
+    }
+
+    public List<string> GetItems()
+    {
+        return _items;
+    }
+}
